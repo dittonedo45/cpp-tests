263bae7bc5f2a587d27116fb5f014575e3001fbb
Eddington Chamunorwa <dittonedo45@gmail.com>
2022-09-26 11:56:28 +0200

    Try libgit, create a "is gitrepo" checker.

--- accountant.cpp
+++ accountant.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 extern "C" {
 #include <ruby/ruby.h>
+#include <git2.h>
 };
 
 using namespace std;
@@ -8,6 +9,7 @@ using namespace std;
 int main (int argsc, char **args)
 {
 	ruby_init ();
+	git_libgit2_init ();
 	ruby_init_loadpath ();
 
 	rb_define_global_function ("fun",(VALUE(*)(...))
@@ -15,14 +17,23 @@ int main (int argsc, char **args)
 {
 	if (argsc==0)
 		rb_raise (rb_eException, "!args not enough");
-	cout << string (RSTRING_PTR (rb_obj_as_string (args[0]))) << endl;
-	return Qnil;
+	git_repository *repo=0;
+	int ret=0;
+
+	ret = git_repository_open (&repo, RSTRING_PTR (rb_obj_as_string (args[0])));
+	if (ret) 
+	{
+		return Qfalse;
+	}
+	git_repository_free (repo);
+	return true;
 }, -1);
 
 	int s;
 	rb_load_protect (rb_str_new_cstr (args[1]),
 			0, &s);
 
+	git_libgit2_shutdown ();
 	ruby_finalize ();
 	return( 0 );
 }
