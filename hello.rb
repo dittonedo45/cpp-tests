93c1c94b779a57a58c1d99b14ced1c241670ccb3
Eddington Chamunorwa <dittonedo45@gmail.com>
2022-09-26 12:23:08 +0200

    Hong pong.

--- hello.rb
+++ hello.rb
@@ -1,14 +1,25 @@
 def rec(a)
-  DIR["#{a}/*"].each do
+  Dir["#{a}/*"].each do
     |x|
     begin
-      (yield "#{a}/#{x}") if (fun "#{a}/#{x}")
+      (yield "#{x}") if (fun "#{x}")
     rescue => e
+      p e
+      next
     end
   end
 end
 
 rec("..") do
   |x|
-  p x
+  next if (File.exists? "#{x}/.git") and (File.file? "#{x}/.git")
+  ((Dir["#{x}/.git/refs/remotes/*/*"].map {|x| File.basename x}) &
+   (Dir["#{x}/.git/refs/heads/*"].map {|x| File.basename x})).each do
+    |y|
+    if not (File.read "#{x}/.git/refs/remotes/origin/#{y}")==
+        (File.read "#{x}/.git/refs/heads/#{y}") then
+    puts "--"
+    end
+  end
+  #//p (File.realpath x)
 end
